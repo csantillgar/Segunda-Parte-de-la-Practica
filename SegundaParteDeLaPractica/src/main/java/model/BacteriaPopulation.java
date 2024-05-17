e639331a5b9a9ba6bae14bf693af75a79c0bdf07
csantillgar <csantill@myuax.com>
2024-05-17 21:52:49 +0200

    commit

--- SegundaParteDeLaPractica/src/main/java/model/BacteriaPopulation.java
+++ SegundaParteDeLaPractica/src/main/java/model/BacteriaPopulation.java
@@ -1,4 +1,38 @@
 package model;
 
-public class BacteriaPopulation {
+import java.time.LocalDate;
+
+public class BacteriaPopulation implements Comparable<BacteriaPopulation> {
+    private String name;
+    private LocalDate startDate;
+    private int initialBacteriaCount;
+    private FoodPattern foodPattern;
+
+    public BacteriaPopulation(String name, LocalDate startDate, int initialBacteriaCount, FoodPattern foodPattern) {
+        this.name = name;
+        this.startDate = startDate;
+        this.initialBacteriaCount = initialBacteriaCount;
+        this.foodPattern = foodPattern;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public int getInitialBacteriaCount() {
+        return initialBacteriaCount;
+    }
+
+    public FoodPattern getFoodPattern() {
+        return foodPattern;
+    }
+
+    @Override
+    public int compareTo(BacteriaPopulation other) {
+        return this.startDate.compareTo(other.startDate);
+    }
 }
