8e84e11a30d592adf6ac7d06255d7814e16e7900
csantillgar <csantill@myuax.com>
2024-05-18 11:14:36 +0200

    commit

--- SegundaParteDeLaPractica/src/main/java/storage/ExperimentSaver.java
+++ SegundaParteDeLaPractica/src/main/java/storage/ExperimentSaver.java
@@ -1,4 +1,31 @@
 package storage;
 
+import model.BacteriaPopulation;
+import model.Experiment;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class ExperimentSaver {
+
+    public void saveExperiment(Experiment experiment, String filePath) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+        writer.write(experiment.getName());
+        writer.newLine();
+        writer.write(experiment.getStartDate().toString());
+        writer.newLine();
+        writer.write(experiment.getEndDate().toString());
+        writer.newLine();
+
+        for (BacteriaPopulation population : experiment.getPopulations()) {
+            writer.write(population.getName() + ",");
+            writer.write(population.getStartDate().toString() + ",");
+            writer.write(population.getInitialBacteriaCount() + ",");
+            writer.write(population.getFoodPattern().name());
+            writer.newLine();
+        }
+
+        writer.close();
+    }
 }
