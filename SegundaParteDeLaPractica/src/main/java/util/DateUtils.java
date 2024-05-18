5d135d261c53bbc305216a947e8b116e6ac6ed53
csantillgar <csantill@myuax.com>
2024-05-18 11:14:59 +0200

    commit

--- SegundaParteDeLaPractica/src/main/java/util/DateUtils.java
+++ SegundaParteDeLaPractica/src/main/java/util/DateUtils.java
@@ -1,4 +1,17 @@
 package util;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class DateUtils {
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+    public static String formatDate(LocalDate date) {
+        return date.format(formatter);
+    }
+
+    public static LocalDate parseDate(String date) {
+        return LocalDate.parse(date, formatter);
+    }
 }
