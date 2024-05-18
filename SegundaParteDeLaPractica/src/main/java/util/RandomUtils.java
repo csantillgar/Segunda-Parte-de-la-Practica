822ce5b038efbe7986d744000fd2a75b37ee32d1
csantillgar <csantill@myuax.com>
2024-05-18 11:15:39 +0200

    commit

--- SegundaParteDeLaPractica/src/main/java/util/RandomUtils.java
+++ SegundaParteDeLaPractica/src/main/java/util/RandomUtils.java
@@ -1,4 +1,12 @@
 package util;
 
+import java.util.Random;
+
 public class RandomUtils {
+
+    private static final Random random = new Random();
+
+    public static int getRandomInt(int min, int max) {
+        return random.nextInt((max - min) + 1) + min;
+    }
 }
