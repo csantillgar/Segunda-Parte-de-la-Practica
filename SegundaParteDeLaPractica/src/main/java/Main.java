e40d5c4c62b92045210d8aadcf951bdeb74ebf1f
csantillgar <csantill@myuax.com>
2024-05-18 11:17:12 +0200

    commit

--- SegundaParteDeLaPractica/src/main/java/Main.java
+++ SegundaParteDeLaPractica/src/main/java/Main.java
@@ -1,2 +1,29 @@
+import model.Experiment;
+import model.MonteCarloSimulation;
+import storage.ExperimentLoader;
+import InterfazUsuario.MainFrame;
+
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            ExperimentLoader loader = new ExperimentLoader();
+            Experiment experiment = loader.loadExperiment("src/main/resources/experiments/example_experiment.txt");
+
+            MonteCarloSimulation simulation = new MonteCarloSimulation();
+            for (var population : experiment.getPopulations()) {
+                simulation.initialize(population);
+                simulation.runSimulation(30, population.getFoodPattern());
+            }
+
+            SwingUtilities.invokeLater(() -> {
+                MainFrame mainFrame = new MainFrame();
+                mainFrame.setVisible(true);
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
