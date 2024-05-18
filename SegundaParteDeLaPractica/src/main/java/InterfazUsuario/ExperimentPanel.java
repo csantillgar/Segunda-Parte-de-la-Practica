078995f02f5e0972f8a0dc4a6a910401b701edbb
csantillgar <csantill@myuax.com>
2024-05-18 11:18:38 +0200

    commit

--- SegundaParteDeLaPractica/src/main/java/InterfazUsuario/ExperimentPanel.java
+++ SegundaParteDeLaPractica/src/main/java/InterfazUsuario/ExperimentPanel.java
@@ -1,4 +1,15 @@
 package InterfazUsuario;
 
-public class ExperimentPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class ExperimentPanel extends JPanel {
+
+    public ExperimentPanel() {
+        setLayout(new BorderLayout());
+
+        // Aquí podemos añadir componentes para gestionar los experimentos
+        JLabel label = new JLabel("Panel de Experimentos");
+        add(label, BorderLayout.CENTER);
+    }
 }
--- SegundaParteDeLaPractica/src/main/java/InterfazUsuario/PopulationPanel.java
+++ SegundaParteDeLaPractica/src/main/java/InterfazUsuario/PopulationPanel.java
@@ -1,4 +1,15 @@
 package InterfazUsuario;
 
-public class PopulationPanel {
+import javax.swing.*;
+import java.awt.*;
+
+public class PopulationPanel extends JPanel {
+
+    public PopulationPanel() {
+        setLayout(new BorderLayout());
+
+        // Aquí podemos añadir componentes para gestionar las poblaciones de bacterias
+        JLabel label = new JLabel("Panel de Poblaciones");
+        add(label, BorderLayout.CENTER);
+    }
 }
