be2df9534a19539581ecf9cd93ad676c286720b7
csantillgar <csantill@myuax.com>
2024-05-18 11:17:51 +0200

    commit

--- SegundaParteDeLaPractica/src/main/java/InterfazUsuario/MainFrame.java
+++ SegundaParteDeLaPractica/src/main/java/InterfazUsuario/MainFrame.java
@@ -1,4 +1,23 @@
 package InterfazUsuario;
 
-public class MainFrame {
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        setTitle("Simulación de Crecimiento Bacteriano");
+        setSize(800, 600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        // Aquí añadimos los paneles para la interfaz de usuario
+        ExperimentPanel experimentPanel = new ExperimentPanel();
+        PopulationPanel populationPanel = new PopulationPanel();
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, experimentPanel, populationPanel);
+        splitPane.setDividerLocation(400);
+
+        add(splitPane, BorderLayout.CENTER);
+    }
 }
